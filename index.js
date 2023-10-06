42e664e99a49f4f313a9308e88e59175b0a6313e
hsanch13 <hayleysanchez13@gmail.com>
2023-10-06 00:06:44 -0600

    added cat functions

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+   
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop("Garfield");
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift("Bob");
+}
+
+function appendCat() {
+    const copyCats = [...cats, "Broom"];
+    return copyCats;
+}
+
+function prependCat() {
+    const copyCat2 = ["Arnold", ...cats];
+    return copyCat2;
+}
+
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+    
+}
